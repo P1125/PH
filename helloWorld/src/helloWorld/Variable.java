dbd8a9e59f0b11d230fa7e4099a104991761f08f
admin <admin@YD03-13>
2020-11-13 12:56:53 +0900

    첫번째

---
+++ helloWorld/src/helloWorld/Variable.java
@@ -0,0 +1,27 @@
+package helloWorld;
+
+public class Variable {
+
+	public static void main(String[] args) {
+//int 타입의 num1-5 num2-10
+
+		int num1 = 5; 
+		int num2 = 10; // -5를 
+		int sum = num1 + num2;
+		int diff = num1 - num2;
+
+		boolean trueOfFalse = sum > diff;
+
+		if(trueOfFalse) {
+			System.out.println("결과 참입니다");
+			
+		}else { 
+			System.out.println("결과 거짓입니다");
+		}
+		//설치 (jdk,eclipse, notepad++)설치후 => enc
+		//c:/Dev/workspace
+	int sum1 = 10 + 20;
+	System.out.println(10 + 20 + " sum " + ( sum + 10) );
+	}
+	
+}
