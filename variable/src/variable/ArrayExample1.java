4055a5f72a07ab6caf7529eaf17282674b95b32e
admin <admin@YD03-13>
2020-11-17 12:43:42 +0900

    배열까지..

--- variable/src/variable/ArrayExample1.java
+++ variable/src/variable/ArrayExample1.java
@@ -1,5 +1,8 @@
 package variable;
 
 public class ArrayExample1 {
-
+public static void main(String[] args) {
+	int[] nubers = { 10,20,30,40,50,60,70,80,90,100};//정수를담을수있는 넘버즈 
+ //1,3,5,7,9번째 값만 더하는 프로그램 홀수 위치만  
+}
 }
